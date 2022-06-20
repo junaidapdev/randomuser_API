d65f91e903374f8e38d4051e023320ce8f29b1a9
junaidapdev <junaidap.dev.com>
2022-06-20 14:17:26 +0530

    API Integrated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,  { useEffect, useState } from "react";
+import {Container, Row, Col, Card} from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+import Axios from "axios"
+import MyCard from './Card'
 
 function App() {
+const [details, setDetails] = useState({});
+
+const fetchDetails = async () => {
+    const {data} = await Axios.get("https://randomuser.me/api/")
+
+    const details = data.results[0];
+
+    setDetails(details);
+
+}
+
+useEffect(() => {
+  fetchDetails();
+}, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-4 bg-primary App">
+       <Row>
+        <Col className="offset-md-4 mt-4" md={4}>
+          <MyCard details={details}/>
+        </Col>
+       </Row>
+    </Container>
+    
   );
 }
 
